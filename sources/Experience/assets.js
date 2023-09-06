0cc6a3db02e9f65276b6512bd7460701c3c91da6
AT010303 <101562874+AT010303@users.noreply.github.com>
2023-09-06 10:02:37 +0530

    first commit

---
+++ sources/Experience/assets.js
@@ -0,0 +1,10 @@
+export default [
+    {
+        name: 'base',
+        data: {},
+        items:
+        [
+            { name: 'paternTexture', source: '/assets/color.jpg', type: 'texture' },
+        ]
+    }
+]
